5cb8ac6608d5ff4b9421cf1571cb8445b24fc043
BhotiaDEV <gaurav.pal1142@gmail.com>
2023-03-21 22:53:04 +0530

    Second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,51 +9,55 @@ const Home = () => {
     
     const [toggleSeemore,setToggleSeemore] = useState([])
     const [toggle,setToggle] = useState(null)
-    const [toggleThread,setToggleThread] = useState(null)
+    const [toggleNotice,setNotice] = useState(false)
+    const [toggleJourney,setJourney] = useState(false)
+    const [categoryToggle,setCategory] = useState(false)
+    const [processToggle,setProcess] = useState(false)
+
 
    
     
     const displayList = (data,title)=>{
         if(data){
-            return(data.filter((item)=> {return item.asset.asset_title === title})
+            return  (data.filter((item)=> {return item.asset.asset_title === title})
                         .map((item,index)=>(
-                <div className='toggle-feature' key={index}>
-                    <button type='button' 
-                            onClick={()=>{
-                                setToggle((prev)=>
-                                    prev === index ? null : index)
-                                }}
-                            className='toggle-heading'>
-                        <i className={toggle === index ? "fa-solid fa-chevron-up" : "fa-solid fa-chevron-down"}></i> &nbsp;{item.title}
-                    </button> 
-                    {toggle === index ?
-                    <div className='toggle-body'>
-                        <p className='toggle-content'>
-                            {item.content}
-                        </p>
-                        {toggleSeemore === index ? 
-                            <p className='toggle-content'>
-                                <br/>
-                                {item.summary}
-                            </p>
-                        :
-                        <></>
-                        }
-                        <button onClick={()=>{
-                            setToggleSeemore((prev)=>
-                            prev === index ? null : index)
-                        }} 
-                        type='button' 
-                                className='see-more'>
-                                See More
-                        </button>
-                    </div> 
-                    :
-                    <></>
-                    } 
-                </div>
-                ))
-            )
+                                <div className='toggle-feature' key={index}>
+                                    <button type='button' 
+                                            onClick={()=>{
+                                                setToggle((prev)=>
+                                                    prev === index ? null : index)
+                                                }}
+                                            className='toggle-heading'>
+                                        <i className={toggle === index ? "fa-solid fa-chevron-up" : "fa-solid fa-chevron-down"}></i> &nbsp;{item.title}
+                                    </button> 
+                                    {toggle === index ?
+                                    <div className='toggle-body'>
+                                        <p className='toggle-content'>
+                                            {item.content}
+                                        </p>
+                                        {toggleSeemore === index ? 
+                                            <p className='toggle-content'>
+                                                <br/>
+                                                {item.summary}
+                                            </p>
+                                        :
+                                        <></>
+                                        }
+                                        <button onClick={()=>{
+                                            setToggleSeemore((prev)=>
+                                            prev === index ? null : index)
+                                        }} 
+                                        type='button' 
+                                                className='see-more'>
+                                                See More
+                                        </button>
+                                    </div> 
+                                    :
+                                    <></>
+                                    } 
+                                </div>
+                            ))
+                    )
         }
     }
     const handleSubmit = (e,asset)=>{
@@ -112,27 +116,6 @@ const Home = () => {
                             >
                             </iframe>
                         : <></>}
-                        {asset.asset_content_type === 'article' ? 
-                            <div className='article-body'>
-                                <form onSubmit={
-                                    (e)=>handleSubmit(e,asset)
-                                    }>
-                                    <div className='article-list'>
-                                        {displayList(article,asset.asset_title)}
-                                    </div>
-                                    <div className='article-heading'>
-                                        <h4>Title</h4>
-                                        <input className='article-title-input' type="text" name='title'  required/>
-                                    </div>
-                                    <div className='article-content'>
-                                        <h4>Content</h4>
-                                        <input className='article-content-input' type="text" name='content'  required/>
-                                    </div>
-                                    <button className='submit-article'>Submit</button>
-                                </form>
-                            </div>
-                        : <></> 
-                        }
                         {asset.asset_content_type === 'threadbuilder' ? 
                             <div className='thread-body'>
                                 <form onSubmit={
@@ -149,6 +132,25 @@ const Home = () => {
                                         <h4>Sub interpretation 1</h4>
                                         <input className='thread-input' type="text" name='interpretation' placeholder='Enter Text here' required/>
                                     </div>
+                                    <div className='threadbuilder-tools'>
+                                        <ul>
+                                            <li>
+                                            <i class="fa-xl fa-solid fa-lightbulb"></i>
+                                            </li>
+                                            <li>
+                                                <i class="fa-xl fa-solid fa-message"></i>
+                                            </li>
+                                        </ul>
+                                        <div className='select-category'>
+                                            Select Category&nbsp;
+                                            <i class="fa-lg fa-solid fa-angle-down"></i>
+                                            <></>
+                                        </div>
+                                        <div className='select-process'>
+                                            Select Process&nbsp;
+                                            <i class="fa-lg fa-solid fa-angle-down"></i>
+                                        </div>
+                                    </div>
                                     <button className='submit-thread'>+&nbsp;Sub-thread</button>
                                     <div className='thread-summary'>
                                         <h4>Summary for Thread A</h4>
@@ -158,11 +160,126 @@ const Home = () => {
                             </div>
                         : <></> 
                         }
+                        {asset.asset_content_type === 'article' ? 
+                            <div className='article-body'>
+                                {asset.asset_type === 'display_asset' ?
+                                    <iframe className='article-iframe' src={asset.asset_content} width="100%" height="800"></iframe>
+                                :
+                                    <form onSubmit={
+                                        (e)=>handleSubmit(e,asset)
+                                    }>
+                                        <div className='article-list'>
+                                            {displayList(article,asset.asset_title)}
+                                        </div>
+                                        <div className='article-heading'>
+                                            <h4>Title</h4>
+                                            <input className='article-title-input' type="text" name='title'  required/>
+                                        </div>
+                                        <div className='article-content'>
+                                            <h4>Content</h4>
+                                            <div className='tools'>
+                                                <ul className='file-options'>
+                                                    <li>File</li>
+                                                    <li>Edit</li>
+                                                    <li>View</li>
+                                                    <li>Insert</li>
+                                                    <li>Format</li>
+                                                    <li>Tools</li>
+                                                    <li>Table</li>
+                                                    <li>Help</li>
+                                                </ul>
+                                                <ul className='tools-options'>
+                                                    <li>
+                                                    <i class="fa-xl fa-solid fa-arrow-rotate-left"></i>
+                                                    </li>
+                                                    <li>
+                                                    <i class="fa-xl fa-solid fa-arrow-rotate-right"></i>
+                                                    </li>
+                                                    <li>
+                                                        <i class="fa-xl fa-light fa-arrows-maximize"></i>
+                                                    </li>
+                                                    <li><input type="text" placeholder='Paragraph' /></li>
+                                                    <li className='dot'></li>
+                                                    <li className='dot'></li>
+                                                    <li className='dot'></li>
+                                                </ul>
+                                            </div>
+                                            <input className='article-content-input' type="text" name='content'  required/>
+                                        </div>
+                                        <button className='submit-article'>Submit</button>
+                                    </form>
+                                }
+                            </div>
+                            : <></> 
+                        }
                     </div>
                 </div>
             </div>
         ))
         }
+        </div>
+        <div className='floater-btns'>
+            <button>
+                <i class="fa-3x fa-solid fa-question"></i>
+            </button>
+            <button>
+                <i class="fa-3x fa-solid fa-people-group"></i>
+            </button>
+            <button>
+                <i class="fa-3x fa-sharp fa-regular fa-calendar-days"></i>
+            </button>
+        </div>
+        <div className='floater-divs'>
+            {toggleJourney? 
+                <button className='journey-board' onClick={()=>{
+                    setJourney(!toggleJourney)
+                }}>
+                    <p>
+                        Journey board
+                        <i class="fa-solid fa-x"></i>
+                    </p>
+                        <ul className='journey-content'>
+                            <li>
+                                <h4>{data.tasks[0].task_title}</h4>
+                            </li>
+                            {data.tasks[0].assets.map((asset)=>(
+                                <li>{asset.asset_title}</li>
+                            ))}
+                        </ul>
+                </button>
+                :
+                <button className='journey-board'  style={{width:"45px"}}  onClick={()=>{
+                    setJourney(!toggleJourney)
+                }}>
+                    <p>
+                    <i class="fa-solid fa-right-long" style={{color: "#ffffff"}}></i>
+                    </p>
+                    <div className='journey-content'>
+                        <div className='journey-numbers'>1</div>
+                    </div>
+                </button>
+            }
+            {toggleNotice?
+                <button className='notice-board' onClick={()=>{
+                    setNotice(!toggleNotice)
+                }}>
+                <p>
+                    <i class=" fa-xl fa-solid fa-x"></i>
+                    Notice board
+                </p>
+                <div className='notice-content'></div>
+            </button>
+            :
+            <button className='notice-board' onClick={()=>{
+                setNotice(!toggleNotice)
+            }}>
+                <p>
+                    <i class=" fa-xl fa-solid fa-x"></i>
+                    Notice board
+                </p>
+                <div className='notice-content' style={{width:"20vw"}}></div>
+            </button>
+            }
         </div>
     </section>
     </React.Fragment>
